5d97041e360bbdfd48740665b311aeefbc6afd4a
Leonardo <dillonhayutin@kontango.us>
2026-07-17 18:39:41 -0600

    Foundations tutorial: add environment=demo tag to orbit_storage (test autodeploy)

--- stacks/sandbox/main.tf
+++ stacks/sandbox/main.tf
@@ -39,9 +39,10 @@ data "aws_caller_identity" "current" {}
 resource "aws_s3_bucket" "orbit_storage" {
   bucket_prefix = "orbit-storage-"
   tags = {
-    name      = "Orbit Labs Storage"
-    managedBy = "Spacelift"
-    mission   = "First Launch"
-    project   = "Orbit-labs"
+    name        = "Orbit Labs Storage"
+    managedBy   = "Spacelift"
+    mission     = "First Launch"
+    project     = "Orbit-labs"
+    environment = "demo"
   }
 }
